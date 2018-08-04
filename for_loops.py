ec7af2f1a478f52e85ae37eb8a0fefbd24c326ff
ericyanginmay123 <ericyanginmay123@gmail.com>
2018-08-04 22:02:44 +0800

    for loop.

---
+++ for_loops.py
@@ -0,0 +1,16 @@
+for letter in "Giraffe Academy":
+    print(letter)
+
+friends = ["Tom","Kate","Green"]
+for friend in friends:
+    print(friend)
+
+for i in range(10):
+    print(i)
+
+for j in range(len(friends)):
+    print(friends[j])
+
+dic = {1:"one",2:"two",3:"three"}
+for key,word in dic.items():
+    print(key,word)
